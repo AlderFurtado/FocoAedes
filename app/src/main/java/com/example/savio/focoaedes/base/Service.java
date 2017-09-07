e1de08c220ea58bffa828a4b5ad5a70468243984
SavioGuedes <savioguedess@gmail.com>
2017-09-07 02:10:46 -0300

    toma

--- app/src/main/java/com/example/savio/focoaedes/base/Service.java
+++ app/src/main/java/com/example/savio/focoaedes/base/Service.java
@@ -17,7 +17,7 @@ import retrofit2.http.Path;
 
 public interface Service {
 
-    public static final String base_url = "http://104.236.7.68/api/v1/";
+    public static final String base_url = "http://104.236.7.68:8080/ocorrencia/";
 
     Gson gson = new GsonBuilder().setLenient().create();
 
@@ -30,16 +30,16 @@ public interface Service {
     //@GET("followers")
     //Call<Catalogos> listaCatalogos1(); //JSON OBJECT
 
-    @GET("ocorrencias")
+    @GET("ocorrencia")
     Call<List<Ocorrencia>> getOcorrencias(); //capturar todas as ocorrencias
 
-    @GET("ocorrencias/{id}")
+    @GET("ocorrencium/{id}")
     Call<List<Ocorrencia>> showOcorrencias(); //Detalhes da ocorrencia
 
-    @POST("ocorrencias")
+    @POST("ocorrencia")
     Call<Ocorrencia> setOcorrencias(@Body Ocorrencia ocorrencias); //postar uma ocorrencia
 
-    @DELETE("ocorrencias/{id}")
+    @DELETE("ocorrencia/{id}")
     Call<Void> deleteOcorrencias(@Path("id") String itemId); //deletar uma ocorrencia
 
 }
--- app/src/main/java/com/example/savio/focoaedes/fragments/Detalhes_OcorrenciaFragment.java
+++ app/src/main/java/com/example/savio/focoaedes/fragments/Detalhes_OcorrenciaFragment.java
@@ -6,12 +6,17 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.example.savio.focoaedes.R;
 
 public class Detalhes_OcorrenciaFragment extends Fragment {
 
     View view;
+
+    ImageView detalhe_imagem;
+    TextView detalhe_titulo;
 
     public Detalhes_OcorrenciaFragment() {
         // Required empty public constructor
@@ -22,6 +27,9 @@ public class Detalhes_OcorrenciaFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         view = inflater.inflate(R.layout.fragment_detalhes_ocorrencia, container, false);
+
+        detalhe_imagem = (ImageView) view.findViewById(R.id.detalhe_imagem);
+        detalhe_titulo = (TextView) view.findViewById(R.id.detalhe_titulo);
 
         return view;
     }
